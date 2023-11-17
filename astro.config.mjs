c7eea48c4ab235debe46afdb34752158fdd6f0b5
Massvdev <107349168+Massvdev@users.noreply.github.com>
2023-11-17 14:15:22 -0600

    Update astro.config.mjs
    vercel name fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://neta-astro.vercel.app/'
+      ? 'https://neta-astrosl.vercel.app/'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3000/',
